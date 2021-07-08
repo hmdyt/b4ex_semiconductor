6238b2a7e1367b5a5e7bc4b4d0bf19655957cf42
hmdyt <konnyaku108@gmail.com>
2021-07-08 16:09:42 +0900

    enable to use for week2 hist data

--- src/week1/convertWaveFormToHist.C
+++ src/week1/convertWaveFormToHist.C
@@ -1,4 +1,4 @@
-TH1D* convertWaveFormToHist(TString run_name = "Cs_002", bool use_cache = true){
+TH1D* convertWaveFormToHist(TString run_name = "Cs_002", bool use_cache = true, bool is_alpha_ray = false){
     // init const
     const TString DATA_PATH = "/data/hamada/semiconductor/week1/";
 
@@ -14,7 +14,7 @@ TH1D* convertWaveFormToHist(TString run_name = "Cs_002", bool use_cache = true){
         run_name + ";ADC value;Event",
         1024,
         0,
-        2048 * 1.5
+        is_alpha_ray ? 100 : 2048 * 1.5
     );
 
     // fetch waveforms
